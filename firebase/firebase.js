4bc4c93a552ef130b5d7313501cad913a0708aaa
zadigg <62830716+zadigg@users.noreply.github.com>
2022-03-04 16:20:20 +0300

    this is the final one

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDWXGaMAPUIb4ZzgYx8nXVpm5SUqNP3LnM",
-  authDomain: "next-crud-c87d5.firebaseapp.com",
-  projectId: "next-crud-c87d5",
-  storageBucket: "next-crud-c87d5.appspot.com",
-  messagingSenderId: "202286618965",
-  appId: "1:202286618965:web:d8d4f0a37c27dd74f4b7dc",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
