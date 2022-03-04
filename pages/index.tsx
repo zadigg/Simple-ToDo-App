3d312b2056be458b86f96fe2918a449cacc69c78
zadigg <62830716+zadigg@users.noreply.github.com>
2022-03-04 16:25:45 +0300

    final after deploy error

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ import {
 import db from "../firebase/firebase";
 
 export default function Home() {
-  const [todos, setTodos] = useState([{ name: "Loading ...", id: "initial" }]);
+  const [todos, setTodos] = useState([{ id: "initial" }]);
 
   // console.log(todos);
 
